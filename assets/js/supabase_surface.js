616e37bbca7b22d8789f633be5f54fd594e991fb
Patrick Muehlbauer <patrick@opentrash.org>
2021-05-26 13:50:28 +0200

    Don't create a liveSocket, this has to happen in the app

--- assets/js/supabase_surface.js
+++ assets/js/supabase_surface.js
@@ -14,31 +14,12 @@ import "../css/app.scss";
 //
 import "alpinejs";
 import "phoenix_html";
-import { Socket } from "phoenix";
 import topbar from "topbar";
-import { LiveSocket } from "phoenix_live_view";
 import Hooks from "./_hooks";
-
-let csrfToken = document
-  .querySelector("meta[name='csrf-token']")
-  .getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {
-  params: { _csrf_token: csrfToken },
-  hooks: Hooks
-});
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", (info) => topbar.show());
 window.addEventListener("phx:page-loading-stop", (info) => topbar.hide());
-
-// connect if there are any LiveViews on the page
-liveSocket.connect();
-
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket;
 
 export { Hooks };
