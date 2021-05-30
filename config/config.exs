a6c768d6f17baea68f2aeb080cc359af1116b6ba
Patrick Muehlbauer <patrick@opentrash.org>
2021-05-30 12:40:17 +0200

    Add title and subtitle for catalogue

--- config/config.exs
+++ config/config.exs
@@ -15,6 +15,10 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :surface_catalogue,
+  title: "Supabase UI",
+  subtitle: "Surface Components for Supabase"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
