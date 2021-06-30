e3e2d46d3d661a4b8235bb25fb6781d5a7af6b18
Patrick Muehlbauer <patrick@opentrash.org>
2021-06-30 22:17:37 +0200

    Run formatter

--- priv/catalogue/components/badge.ex
+++ priv/catalogue/components/badge.ex
@@ -8,22 +8,22 @@ defmodule SupabaseSurface.Components.Badge.Basic do
     catalogue: SupabaseSurface.Catalogue,
     height: "200px"
 
-    alias SupabaseSurface.Components.Badge
+  alias SupabaseSurface.Components.Badge
 
-    def render(assigns) do
-      ~F"""
-      <div class="dark p-8 bg-dark-700">
-        <Badge color="blue">blue</Badge>
-        <Badge color="gray">gray</Badge>
-        <Badge color="green">green</Badge>
-        <Badge color="indigo">indigo</Badge>
-        <Badge color="pink">pink</Badge>
-        <Badge color="purple">purple</Badge>
-        <Badge color="red">red</Badge>
-        <Badge color="yellow">yellow</Badge>
-      </div>
-      """
-    end
+  def render(assigns) do
+    ~F"""
+    <div class="dark p-8 bg-dark-700">
+      <Badge color="blue">blue</Badge>
+      <Badge color="gray">gray</Badge>
+      <Badge color="green">green</Badge>
+      <Badge color="indigo">indigo</Badge>
+      <Badge color="pink">pink</Badge>
+      <Badge color="purple">purple</Badge>
+      <Badge color="red">red</Badge>
+      <Badge color="yellow">yellow</Badge>
+    </div>
+    """
+  end
 end
 
 defmodule SupabaseSurface.Components.Badge.Dot do
@@ -36,14 +36,14 @@ defmodule SupabaseSurface.Components.Badge.Dot do
     catalogue: SupabaseSurface.Catalogue,
     height: "200px"
 
-    alias SupabaseSurface.Components.Badge
+  alias SupabaseSurface.Components.Badge
 
-    def render(assigns) do
-      ~F"""
-      <div class="dark p-8 bg-dark-700">
-        <Badge dot color="blue">blue</Badge>
-        <Badge dot size="large" color="pink">pink</Badge>
-      </div>
-      """
-    end
+  def render(assigns) do
+    ~F"""
+    <div class="dark p-8 bg-dark-700">
+      <Badge dot color="blue">blue</Badge>
+      <Badge dot size="large" color="pink">pink</Badge>
+    </div>
+    """
+  end
 end
